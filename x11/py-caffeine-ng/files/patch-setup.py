8f68521c91af79a63d7364c2c5a5d3933b71e747
Hiroki Tagato <tagattie@gmail.com>
2019-02-21 13:22:27 +0900

    Let FreeBSD port handle glib schema file to avoid plist error.

--- x11/py-caffeine-ng/files/patch-setup.py
+++ x11/py-caffeine-ng/files/patch-setup.py
@@ -1,15 +1,23 @@
 --- setup.py.orig	2018-05-17 01:08:07 UTC
 +++ setup.py
-@@ -12,9 +12,10 @@ def get_data_files():
+@@ -7,14 +7,18 @@ from setuptools import find_packages, setup
+ 
+ 
+ def get_data_files():
++    # Let FreeBSD port handle glib schema file to avoid plist error
++    black_list = ['net.launchpad.caffeine.gschema.xml']
+     data_files = []
+ 
      for path, dirs, files in walk('share'):
          target_path = os.path.join('/usr', path)
  
 -        data_files.append((
 -            target_path, [os.path.join(path, f) for f in files]
 -        ))
-+        if files != []:
++        tl = [os.path.join(path, f) for f in files if f not in black_list]
++        if tl != []:
 +            data_files.append((
-+                target_path, [os.path.join(path, f) for f in files]
++                target_path, tl
 +            ))
  
      data_files.append(
