22d1c6cc5310e2e7d2b510d94b3dea0a8a87c1b3
Hiroki Tagato <tagattie@gmail.com>
2022-01-03 16:19:28 +0900

    Mk/Uses/nodejs.mk: add USE_NODEJS feature
    A NodeJS-based software often uses a node package manager (NPM or
    Yarn) for building and/or running the software.
    
    With the USE_NODEJS feature, a porter can specify NPM or Yarn as the
    package manager and add it to *_DEPENDS based on given arguments.

--- Mk/Uses/nodejs.mk
+++ Mk/Uses/nodejs.mk
@@ -7,6 +7,34 @@
 # Default is:   build,run
 # Note:			if you define a version, you must provide run and/or build
 #
+# Variables, which can be set by a port
+#
+# USE_NODEJS:	Indicates a NodeJS package manager the port uses.
+#
+#	Supported package managers are:
+#	npm[:ARGS]	The port uses NPM as the package manager.
+#	yarn[:ARGS]	The port uses Yarn as the package manager.
+#
+#	Valid ARGS:	fetch, extract, patch, build, run, test
+#		fetch:		Indicates the package manager is needed at
+#				fetch time and adds it to FETCH_DEPENDS.
+#		extract:	Indicates the package manager is needed at
+#				extract time and adds it to EXTRACT_DEPENDS.
+#		patch:		Indicates the package manager is needed at
+#				patch time and adds it to PATCH_DEPENDS.
+#		build:		Indicates the package manager is needed at
+#				build time and adds it to BUILD_DEPENDS.
+#		run:		Indicates the package manager is needed at
+#				run time and adds it to RUN_DEPENDS.
+#		test:		Indicates the package manager is needed at
+#				test time and adds it to TEST_DEPENDS.
+#		Note: If the port specifies none of them, we assume the port
+#		requires the package manager at build time only.
+#
+# Exported variables:
+#
+# NODEJS_PACKAGE_MANAGER:	The chosen NodeJS package manager.
+#
 # MAINTAINER: bhughes@FreeBSD.org
 
 .if !defined(_INCLUDE_USES_NODEJS_MK)
@@ -56,5 +84,33 @@ RUN_DEPENDS+=	node:www/node${_NODEJS_VERSION_SUFFIX}
 . if ${nodejs_ARGS:M*build*}
 BUILD_DEPENDS+=	node:www/node${_NODEJS_VERSION_SUFFIX}
 . endif
+
+.if defined(USE_NODEJS)
+_VALID_NODEJS_FEATURE=	npm yarn
+
+_NODEJS_FEATURE=	${USE_NODEJS:C/:.*//}
+_NODEJS_FEATURE_ARGS=	${USE_NODEJS:C/^[^:]*(:|\$)//}
+
+. if !${_VALID_NODEJS_FEATURE:M${_NODEJS_FEATURE}}
+IGNORE=	specifies unknown USE_NODEJS feature: ${_NODEJS_FEATURE}
+. endif
+. if empty(_NODEJS_FEATURE_ARGS)
+_NODEJS_FEATURE_ARGS=	build
+. endif
+
+. if empty(_NODEJS_VERSION_SUFFIX)
+_NODEJS_FEATURE_DEP=	www/${_NODEJS_FEATURE}>0:www/${_NODEJS_FEATURE}
+. else
+_NODEJS_FEATURE_DEP=	www/${_NODEJS_FEATURE}-node${_NODEJS_VERSION_SUFFIX}>0:www/${_NODEJS_FEATURE}-node${_NODEJS_VERSION_SUFFIX}
+. endif
+
+. for phase in fetch extract patch build run test
+.  if ${_NODEJS_FEATURE_ARGS:M*${phase}*}
+${phase:tu}_DEPENDS+=	${_NODEJS_FEATURE_DEP}
+.  endif
+. endfor
+
+NODEJS_PKG_MANAGER=	${_NODEJS_FEATURE}
+.endif # USE_NODEJS
 
 .endif
