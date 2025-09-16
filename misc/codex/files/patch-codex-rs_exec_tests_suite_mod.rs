0a4ab1774e227188cbe1e40e8593cc8ae45e8889
Hiroki Tagato <tagattie@gmail.com>
2025-09-16 11:48:33 +0900

    misc/codex: update to 0.36.0
    Changelog:
    - https://github.com/openai/codex/releases/tag/rust-v0.35.0
    - https://github.com/openai/codex/releases/tag/rust-v0.36.0

--- misc/codex/files/patch-codex-rs_exec_tests_suite_mod.rs
+++ misc/codex/files/patch-codex-rs_exec_tests_suite_mod.rs
@@ -1,8 +1,8 @@
---- codex-rs/exec/tests/suite/mod.rs.orig	2025-09-11 03:59:06 UTC
+--- codex-rs/exec/tests/suite/mod.rs.orig	2025-09-15 17:04:50 UTC
 +++ codex-rs/exec/tests/suite/mod.rs
-@@ -1,4 +1,5 @@ mod common;
- // Aggregates all former standalone integration tests as modules.
+@@ -2,4 +2,5 @@ mod resume;
  mod apply_patch;
  mod common;
+ mod resume;
 +#[cfg(not(target_os = "freebsd"))]
  mod sandbox;
