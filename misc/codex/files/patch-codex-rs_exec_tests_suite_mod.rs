1cfc576093ec144ae0f8b0a51829078450eda8f6
Hiroki Tagato <tagattie@gmail.com>
2025-09-26 11:01:14 +0900

    misc/codex: update to 0.41.0
    While here, add rg to runtime dependency.
    
    Changelog: https://github.com/openai/codex/releases/tag/rust-v0.41.0

--- misc/codex/files/patch-codex-rs_exec_tests_suite_mod.rs
+++ misc/codex/files/patch-codex-rs_exec_tests_suite_mod.rs
@@ -1,8 +1,8 @@
---- codex-rs/exec/tests/suite/mod.rs.orig	2025-09-15 17:04:50 UTC
+--- codex-rs/exec/tests/suite/mod.rs.orig	2025-09-24 16:56:31 UTC
 +++ codex-rs/exec/tests/suite/mod.rs
-@@ -2,4 +2,5 @@ mod resume;
- mod apply_patch;
+@@ -3,4 +3,5 @@ mod resume;
  mod common;
+ mod output_schema;
  mod resume;
 +#[cfg(not(target_os = "freebsd"))]
  mod sandbox;
